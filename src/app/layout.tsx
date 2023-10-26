2e16a3d449eb29c2d8b13307b7ab34be758463da
gustavolimapereira <gu.lim@hotmail.com>
2023-10-26 11:40:38 -0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
           <div className="bg-yellow-400">9</div>
         </div>
         <button className="bg-blue-500 hover:bg-blue-700 active:scale-95 transform transition-all duration-100 ease-in-out text-white font-bold py-2 px-4 rounded">
-          Clique Aqui maldita
+          Clique Aqui
         </button>
       </body>
     </html>
