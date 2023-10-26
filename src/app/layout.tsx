975dbb66a8700dafc9fd5bb28a842e4bcf33d863
gustavolimapereira <gu.lim@hotmail.com>
2023-10-26 11:26:24 -0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <div className="grid grid-cols-3 gap-2 p-5 h-auto bg-pink-400">
           <div className="bg-green-400 h-80 flex items-center justify-center flex-col">
             <p className="font-sans">Gustavo Lima</p>
-            <p className="font-serif">Gustavo Lima</p>
+            <p className="font-serif">Carolina Infante</p>
           </div>
           <div className="bg-red-400">
             <p className="font-thin">Gustavo Lima</p>
@@ -28,7 +28,7 @@ export default function RootLayout({
           <div className="bg-yellow-400">9</div>
         </div>
         <button className="bg-blue-500 hover:bg-blue-700 active:scale-95 transform transition-all duration-100 ease-in-out text-white font-bold py-2 px-4 rounded">
-          Clique Aqui
+          Clique Aqui maldita
         </button>
       </body>
     </html>
