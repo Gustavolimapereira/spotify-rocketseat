9ed90099037761cc400985e3f759ad42daf8d7ea
gustavolimapereira <gu.lim@hotmail.com>
2023-10-26 10:47:58 -0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,22 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className="bg-zinc-800">teste1</body>
-      <text className="text-red-800">gustavo</text>
+    <html lang="pt-br">
+      <body className="bg-zinc-800 h-100 w-full">
+        <div className="grid grid-cols-3 gap-2 p-5 h-auto bg-pink-400">
+          <div className="bg-green-400 h-80 flex items-center justify-center flex-col">
+            <p className="font-sans">Gustavo Lima</p>
+            <p className="font-serif">Gustavo Lima</p>
+          </div>
+          <div className="bg-red-400">
+            <p className="font-thin">Gustavo Lima</p>
+          </div>
+          <div className="bg-yellow-400">9</div>
+        </div>
+        <button className="bg-blue-500 hover:bg-blue-700 active:scale-95 transform transition-all duration-100 ease-in-out text-white font-bold py-2 px-4 rounded">
+          Clique Aqui
+        </button>
+      </body>
     </html>
   )
 }
