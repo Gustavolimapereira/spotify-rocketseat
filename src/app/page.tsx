92d4f48b4f9ecbdbc428d6ffe6c988ed598f3416
gustavolimapereira <gu.lim@hotmail.com.br>
2023-10-26 21:34:53 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,22 @@ export default function Home() {
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
         <aside className="w-72 bg-zinc-950 p-6">
-          <nav className="space-y-4">
+          <div className="flex items-center gap-2">
+            <div className="w-3 h-3 bg-red-500 rounded-full" />
+            <div className="w-3 h-3 bg-yellow-500 rounded-full" />
+            <div className="w-3 h-3 bg-green-500 rounded-full" />
+          </div>
+          <nav className="space-y-5 mt-10">
             <a
               href=""
-              className="flex intems-center gap-2 text-xs font-semibold text-zinc-100"
+              className="flex intems-center gap-3 text-sm font-semibold text-zinc-200"
             >
               {' '}
               <HomeIcon /> Home
             </a>
             <a
               href=""
-              className="flex intems-center gap-2 text-xs font-semibold text-zinc-100"
+              className="flex intems-center gap-3 text-sm font-semibold text-zinc-200"
             >
               {' '}
               <Search />
@@ -23,11 +28,43 @@ export default function Home() {
             </a>
             <a
               href=""
-              className="flex intems-center gap-2 text-xs font-semibold text-zinc-100"
+              className="flex intems-center gap-3 text-sm font-semibold text-zinc-200"
             >
               {' '}
               <Library />
               Your Library
+            </a>
+          </nav>
+          <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-2">
+            <a href="" className="text-sm text-zinc-400">
+              PlayList 1
+            </a>
+            <a href="" className="text-sm text-zinc-400">
+              PlayList 2
+            </a>
+            <a href="" className="text-sm text-zinc-400">
+              PlayList 3
+            </a>
+            <a href="" className="text-sm text-zinc-400">
+              PlayList 4
+            </a>
+            <a href="" className="text-sm text-zinc-400">
+              PlayList 5
+            </a>
+            <a href="" className="text-sm text-zinc-400">
+              PlayList 6
+            </a>
+            <a href="" className="text-sm text-zinc-400">
+              PlayList 7
+            </a>
+            <a href="" className="text-sm text-zinc-400">
+              PlayList 8
+            </a>
+            <a href="" className="text-sm text-zinc-400">
+              PlayList 9
+            </a>
+            <a href="" className="text-sm text-zinc-400">
+              PlayList 10
             </a>
           </nav>
         </aside>
